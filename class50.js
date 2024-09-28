33d48f366266c6175e65b2cc9a5cf6c3aae098f7
Tanweer Alam <tanweer@gmail.com>
2024-09-28 23:53:46 +0530

    first commit

---
+++ class50.js
@@ -0,0 +1,64 @@
+/* function reverseString1(str){
+    console.log(str.split('').reverse().join(""))
+}
+function reverseString2(str){
+    return str.split('').reverse().join("")
+}
+ let ans1 =reverseString1("abcd")
+ let ans2 =reverseString2("cba")
+
+ console.log(ans1 ,ans2) */
+
+
+ /* let str ="this"
+ console.log(str.charAt(0).toUpperCase())
+ console.log(str.slice(1)) */
+
+ /* let arr =[4,46,57,56789,658]
+ let arr2=[4,23,53,46,565]
+ console.log(arr.includes(4))
+arr=[]
+
+ for(let i=0;i<arr.length();i++){
+    if(arr2.includes(arr[i]) && !newarr.includes(arr[i])){
+        newarr.push(arr[i])
+    }
+ } */
+
+
+
+
+ /* 
+   const books=[
+    {title:'The Great Gatsby',author: 'F.Scott  Fitzgerald'},
+    {title :'To kill a Mockingbird', author: 'Harper Lee'},
+    {title :'1984',author: 'George Orwell'}
+ ];
+
+ /* let authors =[]
+ books.forEach((value ,index) =>{
+    authors.push(value.author)
+
+ })
+ /* console.log(authors) */
+ /* let author =books.map((value,index)=>{
+    return value.author
+ })
+ console.log(author)  */
+
+
+
+
+ const man =[
+    {name:'Alice',age:25},
+    {name :'Bob',age:30},
+    {name :'Charlie',age:20},
+    {name:'David',age:35}
+ ]
+ let age=[]
+ if(age>=30){
+    con
+ let age =man.map((value,index)=>{
+    return value.age
+ })}
+ console.log(age)
